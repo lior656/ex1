7e40dc324c299926660709a2da025538c3b09c26
lior bar <lior656@gmail.com>
2013-03-02 15:54:15 +0200

    basic work
    text change

--- tipcalculator/src/il/ac/huji/tipcalculator/TipCalculatorActivity.java
+++ tipcalculator/src/il/ac/huji/tipcalculator/TipCalculatorActivity.java
@@ -3,6 +3,10 @@ package il.ac.huji.tipcalculator;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TipCalculatorActivity extends Activity {
 
@@ -10,6 +14,16 @@ public class TipCalculatorActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calculator);
+        
+        Button culcButton = (Button) findViewById(R.id.btnCalculate);
+        final TextView tipRes = (TextView) findViewById(R.id.txtTipResult);
+        culcButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				tipRes.setText("Thanks!");
+				
+			}
+		});
     }
 
 
