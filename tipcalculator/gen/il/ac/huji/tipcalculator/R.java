0812f712608ed4713d0a0b1efc5d0cbb6792f622
lior bar <lior656@gmail.com>
2013-03-02 17:55:42 +0200

    added names to xml string

--- tipcalculator/gen/il/ac/huji/tipcalculator/R.java
+++ tipcalculator/gen/il/ac/huji/tipcalculator/R.java
@@ -24,12 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int TextView01=0x7f080004;
         public static final int action_settings=0x7f080005;
         public static final int btnCalculate=0x7f080003;
-        public static final int chkRound=0x7f080001;
-        public static final int edtBillAmount=0x7f080002;
-        public static final int textView1=0x7f080000;
-        public static final int txtTipResult=0x7f080004;
+        public static final int chkRound=0x7f080000;
+        public static final int edtBillAmount=0x7f080001;
+        public static final int txtTipResult=0x7f080002;
     }
     public static final class layout {
         public static final int activity_tip_calculator=0x7f030000;
@@ -38,9 +38,13 @@ public final class R {
         public static final int tip_calculator=0x7f070000;
     }
     public static final class string {
+        public static final int Bill_Amount=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int calc=0x7f050005;
+        public static final int cheakRound=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int tip=0x7f050006;
     }
     public static final class style {
         /** 
